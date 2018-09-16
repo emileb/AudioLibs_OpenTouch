f4d4cb99d1ded203584ef774d0d3c1883dfe2875
Emile Belanger <emile.belanger@gmail.com>
2018-09-16 20:41:40 +0100

    FMOD does not produce logging version to save space

--- FMOD_studio/Android.mk
+++ FMOD_studio/Android.mk
@@ -8,7 +8,7 @@ LOCAL_SRC_FILES := api/lowlevel/lib/$(TARGET_ARCH_ABI)/libfmod.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 #Logging version
-include $(CLEAR_VARS)
-LOCAL_MODULE    := fmodL
-LOCAL_SRC_FILES := api/lowlevel/lib/$(TARGET_ARCH_ABI)/libfmodL.so
-include $(PREBUILT_SHARED_LIBRARY)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE    := fmodL
+#LOCAL_SRC_FILES := api/lowlevel/lib/$(TARGET_ARCH_ABI)/libfmodL.so
+#include $(PREBUILT_SHARED_LIBRARY)
