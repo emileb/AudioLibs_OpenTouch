fc0632f4d75e108ac474e10199ba4330a8e0ec41
Emile Belanger <emile.belanger@gmail.com>
2026-03-20 18:06:05 +0000

    Updated fluidsynth, added oboe and openal-soft, uses submodules. Framework taken from sandstrangers project thanks

--- openal-soft/Android.mk
+++ openal-soft/Android.mk
@@ -3,10 +3,10 @@ LOCAL_PATH := $(call my-dir)
 # --- 1. Define OpenAL as a prebuilt module ---
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := fluidsynth
+LOCAL_MODULE := openal-soft
 # Point this to where CMake outputs the .so file
 # $(if...) helps handle multiple ABIs automatically
-LOCAL_SRC_FILES := build/intermediates/merged_native_libs/release/mergeReleaseNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libfluidsynth.so
+LOCAL_SRC_FILES := build/intermediates/merged_native_libs/release/mergeReleaseNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libopenal-soft.so
 
 include $(PREBUILT_SHARED_LIBRARY)
 
